ac25b8eb276d843d3a2f09c0c620ee31a3502ba3
MobikTech <62842262+MobikTech@users.noreply.github.com>
2021-12-03 18:31:03 +0200

    cleaning

--- Scripts/Old/Entities/CollidableEntity.py
+++
@@ -1,6 +0,0 @@
-from Scripts.Model.SpriteEntity import SpriteEntity
-
-
-class CollidableEntity():
-    def collisionHandler(self, sprite: SpriteEntity):
-        raise NotImplementedError("This method not implemented")
